25ccc0916b78e0ccbcfb734f881a5ed7ce372f48
buyondoemmanuel <buyondoemmanuel29@gmail.com>
2026-03-24 20:39:41 +0300

    Implement electricity bill calculation
    Added functionality to calculate and display the total electricity bill based on user input.

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,29 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
-
+/*BUYONDO EMMANUEL*/
+/*24/U/BIO/O4517/PD*/
 #include <stdio.h>
+int main () {
+    float unitsConsumed, costPerunit, totalElectricityBill;
+    // Prompt the user for the units consumed and the cost per unit
 
-int main(void)
-{
-    /* Write your code here */
+
+    printf("Enter the units consumed: ");
+    scanf("%f", &unitsConsumed);
+    printf("Enter the cost per unit: ");
+    scanf("%f", &costPerunit);
+
+    // Calculate the total electricity bill
+
+    totalElectricityBill = unitsConsumed * costPerunit;
+
+    // Display the total electricity bill
+
+    printf("The total electricity bill is: %.2f UGX\n", totalElectricityBill);
+    printf("..........Electricity Bill Details..........\n");
+    printf("The units consumed are: %.2f\n", unitsConsumed);
+    printf("The cost per unit is: %.2f UGX\n", costPerunit);
+    printf("\nThank you for using our service!");
+
 
     return 0;
+
 }
