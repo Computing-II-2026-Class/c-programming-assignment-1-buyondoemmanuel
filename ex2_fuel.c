5085eb90fbc3d40cc1f84c8d04c7f3e9d33fbe8b
buyondoemmanuel <buyondoemmanuel29@gmail.com>
2026-03-24 21:55:12 +0300

    Refactor fuel efficiency calculation variables and prompts

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -6,12 +6,12 @@
 int main(void)
 {
 
-    float distance, fuel, fuel_efficiency;
-    printf("Enter the distance traveled in kilometers: ");
-    scanf("%f", &distance);
-    printf("Enter the amount of fuel consumed in liters: ");
-    scanf("%f", &fuel);
-    fuel_efficiency = distance / fuel;
-    printf("Your fuel efficiency is: %.2f km/l", fuel_efficiency);
+    float distance_travelled, fuel_consumed, fuel_efficiency;
+    printf("Enter the distance_traveled in kilometers:.\n ");
+    scanf("%f", &distance_travelled);
+    printf("Enter the amount of fuel_consumed in liters: ");
+    scanf("%f", &fuel_consumed);
+    fuel_efficiency = distance_travelled/ fuel_consumed;
+    printf("Your fuel_efficiency is: %.2f km/l", fuel_efficiency);
     return 0;
 }
