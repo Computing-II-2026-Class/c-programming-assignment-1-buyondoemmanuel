dce1f312dfca69be8c297efeeb2263d7bd7e7489
buyondoemmanuel <buyondoemmanuel29@gmail.com>
2026-03-24 20:36:24 +0300

    Implement fuel efficiency calculation
    Added functionality to calculate fuel efficiency based on distance and fuel consumed.

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,17 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: BUYONDO EMMANUEL*/
+/* Student Number: 24/U/BIO/04517/PD */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
 
+    float distance, fuel, fuel_efficiency;
+    printf("Enter the distance traveled in kilometers: ");
+    scanf("%f", &distance);
+    printf("Enter the amount of fuel consumed in liters: ");
+    scanf("%f", &fuel);
+    fuel_efficiency = distance / fuel;
+    printf("Your fuel efficiency is: %.2f km/l", fuel_efficiency);
     return 0;
 }
