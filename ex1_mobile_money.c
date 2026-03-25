f5927f1153da4dcdcab16bea3d859de6e8194385
buyondoemmanuel <buyondoemmanuel29@gmail.com>
2026-03-25 08:07:49 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -5,31 +5,26 @@
 
 int main(void)
 {
-    float amountTosend, transactionFee, totalAmount;
+    float amountTosend, transactionFee(%%), feeAmount, totalAmount;
     // Prompt the user for the amount to send and the transaction fee percentage
     
     
-    printf("Enter the amount you want to send: ");
+    printf("Enter amountToSend:");
     scanf("%f", &amountTosend);
-    printf("Enter the transaction fee (%%): ");
-    scanf("%f", &transactionFee);
+    printf("Enter transactionFee(%%):");
+    scanf("%f", &transactionFee(%%));
    
     // Calculate the transaction fee and the total amount to be paid
     
-    transactionFee = amountTosend * transactionFee / 100;
-    totalAmount = amountTosend + transactionFee;
+    feeAmount = (amountTosend * transactionFee(%%))/ 100;
+    totalAmount = amountTosend + feeAmount;
    
     // Display the total amount to be paid
    
-    printf("The total amount to be paid is: %.2f UGX\n", totalAmount);
-    printf("..........Transaction Details..........\n");
-    printf("The amount to be sent is: %.2f UGX\n", amountTosend);
-    printf("The transaction fee is: %.2f UGX\n", transactionFee);
-    printf("The amount to be received by the recipient is: %.2f UGX\n", amountTosend);
-    printf("\nThank you for using our service!");
+    printf("feeAmount%.2f\n", feeAmount);
+    printf("totalAmount%.2f\n", totalAmount);
 
-
-    return 0;
+ return 0;
 
 }
 
